5f4d02612d506f3de05957214358f1d9a5cf6fb6
Sumiyasaki <ernesto134alonso123@gmail.com>
2021-06-11 18:59:58 -0500

    Proyecto-CitasApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import Formulario from './Components/Formulario.jsx'
+import Cita from './Components/Cita.jsx'
 
-function App() {
+
+export default function App() {
+  
+  let CitasLocalStorage = JSON.parse(localStorage.getItem('citas'));
+  const [citas, setCitas] = useState(CitasLocalStorage)
+
+  //function delete Cita ID
+  const deleteCita = (id) => {
+    const nuevaCita = citas.filter(cita => cita.id !== id )
+    setCitas(nuevaCita)
+  }
+
+  const CrearCita = (cita) => {
+    setCitas([...citas,cita])
+  }
+
+
+  useEffect(()=>{
+  
+      if(CitasLocalStorage)
+      {
+        localStorage.setItem('citas',JSON.stringify(citas))
+      }else{
+        localStorage.setItem('citas',JSON.stringify([]))
+      }
+
+  },[citas,setCitas])
+
+
+
+  const titulo = citas.length === 0 ? "No hay citas" : `Tiene ${citas.length} citas`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+          <div className="row">
+              <div className="one-half column"> 
+                  <Formulario CrearCita={CrearCita}/>
+              </div>
+              <div className="one-half column">
+                <h2>{titulo}</h2> 
+                  {citas.map(cita =>{
+                    return <Cita
+                        key={cita.id}
+                        cita={cita}
+                        deleteCita={deleteCita}
+                    />
+                  })}
+              </div>
+          </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
